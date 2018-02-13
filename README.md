c310ec6a557404e2ca6239d76a58ff7043375d5d
DatNguyen <datnguyen5653@huskers.unl.edu>
2018-02-13 01:13:19 -0600

    update autonomous v2, added driveFunction.h and driveFunction.c -- progress via README.md

--- README.md
+++ README.md
@@ -17,4 +17,21 @@
 #### Issuee
 <rb> *The front mobile lift is currently not working.(motor issue)
 
----------------------------
+--------------------------------------------------------
+
+## Date: Feb 13rd, 2018
+#### To do:
+<rb> * modify autonomous v2 code </rb>
+<rb> * update cone joint Function if needed.</rb>
+<rb> *need the 2 front mobile lift motor to run at the same rate, up and down.</rb>
+<rb> *Code to test the back cone joint, and pneumatic.- check out my code( autonomous_v1.0.c))</rb>
+<rb> add gyro scope to the bot </rb>
+<rb> upload the v2 autonomous code to the bot, thne test</rb>
+
+#### Done
+<rb> *put limit switch to stop the inner lift</rb>
+
+#### update:
+<rb> ryan's code for the bot </rb>
+<rb> commited task code to support the bot developer.
+<rb> https://github.com/RycoTech/UNL_VEXU_CODE</rb>
--- autonomous_v1.0.c
+++ autonomous_v1.0.c
@@ -1,176 +1,44 @@
 /*
   *Author UNL vex U
-  *Date feb 2nd 2018
-  *Version : 1.0
+  *Date feb 13th 2018
+  *Version : 2.0
   *Note:
   *need to be Add:
   *INCH_PER_TICK, TICK_PER_INCH,WHEEL_DIAMETER,.
-  *edit: innerMobileLift() function
-        rotate() function
-        frontMobileLift() idea . have the potentiometer change to encoder motor
+  *edit: rotate( now with gyro scope)
+        progress on code wise.
 
   */
 
-/* Define variables */
-// constant
-float INCH_PER_TICK
-float TICK_PER_INCH
+task autonomous(){
+   //declare varible
+   bool rightLineReached = false;
+   bool leftLineReached = false;
+   int phase[10] = {};
 
-float WHEEL_DIAMETER
+   goForward(2,80);
+   rotate(45,60);
 
-// relative to the center point to the two wheel
-float CHASSIS_WIDTH
-float CHASSIS_LENGTH
-
-float ONE_RADIAN = PI/180;
-
-
-/*
- *This function convert distance request the robot to travel
- *to number of ticks
- * @ parameter ( float inch, int power of motor)
- * EX: goForward(2.8, 50);
- * Go foward 28 inches with 50 power of the motor.
- */
-void goForward(float inch,int power){
-
-  int tickGoal = inch * TICK_PER_INCH ;
-
-  // start encoder count at 0
-  SensorValue[leftEncoder] = 0;
-  SensorValue[rightEncoder] = 0;
-
-  // go to requested ticks.
-  while(SensorValue[leftEncoder] < tickGoal){
-    motor[leftDrive] = power ;
-    motor[rightDrive] = power ;
-  }
-}
-
-/*
- *this function will rotate the robot
- *@parameter (char axis(L,C,R), int degree, int powe)
- *
-            |--------|
-             |      |
-             |  CM  |
-             |      |
-            |--------|
-EX: rotate('C',200,50);
-  The robot will rotate at the C axis by 200 degrees(CW)
-  with 50 power.
-
-EX: rotate('C',-100,100);
-  The robot will rotate at the C axis by 100 degrees(CCW)
-  with 100 power.
-  *
-  */
- void rotate(int degree,int power){//
-
-   // start encoder count at 0
-   SensorValue[leftEncoder] = 0;
+   //reset Encoder
    SensorValue[rightEncoder] = 0;
 
-
-
-
- }
-
-/**
- *this function actiavate front motor lift.
- *@parameter char position, 'L' (low) 'M'(middle) 'H'(high)
- *return void
- *Ex: frontMobileLift('M');
- * this function will activate the motor to lift up the mobile joint. to the middle
- * position.
- */
- void frontMobileLift(char position){
-   int value = sensor(rightLine);
-   if(position == 'L'){
-     if(sensorValue(liftAngleRight) != -240 ){
-       if(value > -240){
-         motor[liftMobileHigherLeft] = -63;
-        motor[liftMobileHigherRight] = -63;
-      }else if(value < -240){
-        motor[liftMobileHigherLeft] = 63;
-        motor[liftMobileHigherRight] =63;
-      }else{
-        stopMotor(liftMobileHigherLeft);
-        stopMotor(liftMobileHigherRight);
-      }
-     }
-   }else if(position == 'H'){
-     if(sensorValue(liftAngleRight) != 1200 ){
-       if(value > 1200){
-         motor[liftMobileHigherLeft] = -63;
-        motor[liftMobileHigherRight] = -63;
-      }else if(value < 1200){
-        motor[liftMobileHigherLeft] = 63;
-        motor[liftMobileHigherRight] =63;
-      }else{
-        stopMotor(liftMobileHigherLeft);
-        stopMotor(liftMobileHigherRight);
-      }
-     }
+   while(SensorValue[mobileGoalHigher] != 1  &&
+        getDistanceTraveled(SensorValue[rightEncoder]) < PLACE_HOLDER_INCH){
+     motor[driveLeft] = 50;
+     motor[driveRight] = 50;
    }
- }
 
- /**
-  *innerMobileLift(char position)
-  *@parameter 'U' up, 'D' Down
-  *return void
-  *This function need to be edit. we are planning put limit switch instead of
-  *using encoder to stop the inner lift motor
-  *
-  */
-  void innerMobileLift(char position){
-        //reset encoder for motor on both side
-    resetMotorEncoder(liftMobileLowerRight);
-    resetMotorEncoder(liftMobileLowerLeft);
+   frontMobileLift('U');
 
-    if(position == 'U'){
-      while(getMotorEncoder(liftMobileLowerRight) != 1250){ //  planning to change it to bumper switch to detect.
-        if(getMotorEncoder(liftMobileLowerRight) > 1250){
-        motor[liftMobileLowerLeft] = 50 ; // replace power with a number
-        motor[liftMolbileLowerRight] = 50 ; // replace power with a number
-      }else if(getMotorEncoder(liftMobileLowerRight) < 1250){
-        motor[liftMobileLowerLeft] = -50 ; // replace power with a number
-        motor[liftMolbileLowerRight] = -50 ; // replace power with a number
-      }else{
-        stopMotor(liftMolbileLowerRight);
-        stopMotor(liftMobileLowerLeft;
-      }
-    }else if(position == 'D'){
-      while(getMotorEncoder(liftMobileLowerRight) != 100 ){
-        if(getMotorEncoder(liftMobileLowerRight) > 100){
-        motor[liftMobileLowerLeft] = 50 ; // replace power with a number
-        motor[liftMolbileLowerRight] = 50 ; // replace power with a number
-      }else if(getMotorEncoder(liftMobileLowerRight) < 100){
-        motor[liftMobileLowerLeft] = -50 ; // replace power with a number
-        motor[liftMolbileLowerRight] = -50 ; // replace power with a number
-      }else{
-        stopMotor(liftMolbileLowerRight);
-        stopMotor(liftMobileLowerLeft;
-      }
-      }else{
-        // error from programmer
-      }
-    }
-  }// end innerMobileLift
+   /*
+   pseudo:
+        go back a certain distance .
+        rotate CCW then align along the all.
+        straight while inner bump sensor != 1 then go straight and scoop middle goal 
 
-  /*
-   *This function activate the cone lift position.
-   * as well as the pneumatic lift
-   * @parameter char jointPos('R'(ready),'L'(lift),
-   *'LL'(local load),'R'(rest), bool extender . true = out. false = in .
-   */
-  void coneJoint(char jointPos, bool extender){
-    if(jointPos == 'R'){
+   // alignment from adjusting both line sensor
 
-    }else if (jointPos == 'L'){
 
-    }else if(jointPos == '')
-  }
 
- task autonomous(){
+
  }
